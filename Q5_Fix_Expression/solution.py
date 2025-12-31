1ca5bba82f78d2935ab6e762295cac94e3475ac1
M.Huzaifa <huzaifaa.ayyazz@gmail.com>
2025-12-31 19:25:10 +0500

    Q5: Updated to Optimized DFS solution

--- Q5_Fix_Expression/solution.py
+++ Q5_Fix_Expression/solution.py
@@ -2,31 +2,37 @@
 Logic Forge Week 1 - Challenge 5: Fix the Broken Expression
 Author: [Your Name]
 """
-def is_valid(s):
-    count = 0
+def solve_optimized(s):
+    left_rem = 0
+    right_rem = 0
     for char in s:
-        if char == '(': count += 1
-        elif char == ')': count -= 1
-        if count < 0: return False
-    return count == 0
+        if char == '(': left_rem += 1
+        elif char == ')':
+            if left_rem > 0: left_rem -= 1
+            else: right_rem += 1
 
-def solve_brute_force(s):
-    level = {s} 
-    while True:
-        valid_strings = []
-        for elem in level:
-            if is_valid(elem):
-                valid_strings.append(elem)
-        if valid_strings:
-            return sorted(valid_strings)
-        next_level = set()
-        for elem in level:
-            for i in range(len(elem)):
-                if elem[i] in "()":
-                    new_s = elem[:i] + elem[i+1:]
-                    next_level.add(new_s)
-        level = next_level
-        if not level: return [""]
+    result = set()
+    def dfs(index, left_count, right_count, left_rem, right_rem, current_expr):
+        if index == len(s):
+            if left_rem == 0 and right_rem == 0 and left_count == right_count:
+                result.add("".join(current_expr))
+            return
+
+        char = s[index]
+        # Option A: Remove char
+        if (char == '(' and left_rem > 0) or (char == ')' and right_rem > 0):
+            if char == '(': dfs(index + 1, left_count, right_count, left_rem - 1, right_rem, current_expr)
+            elif char == ')': dfs(index + 1, left_count, right_count, left_rem, right_rem - 1, current_expr)
+        
+        # Option B: Keep char
+        current_expr.append(char)
+        if char != '(' and char != ')': dfs(index + 1, left_count, right_count, left_rem, right_rem, current_expr)
+        elif char == '(': dfs(index + 1, left_count + 1, right_count, left_rem, right_rem, current_expr)
+        elif char == ')' and right_count < left_count: dfs(index + 1, left_count, right_count + 1, left_rem, right_rem, current_expr)
+        current_expr.pop()
+
+    dfs(0, 0, 0, left_rem, right_rem, [])
+    return list(result)
 
 if __name__ == "__main__":
-    print(solve_brute_force("()())()"))
+    print(solve_optimized("()())()"))
